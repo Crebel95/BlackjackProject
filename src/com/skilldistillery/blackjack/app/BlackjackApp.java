f52fb18d8fc67f7b73b831a75d117178557deb34
Jacob <jwv928@gmail.com>
2023-04-08 16:52:10 -0400

    updated

--- src/com/skilldistillery/blackjack/app/BlackjackApp.java
+++ src/com/skilldistillery/blackjack/app/BlackjackApp.java
@@ -44,7 +44,6 @@ public class BlackjackApp {
 		while (running) {
 
 			if (player.isBust() || dealer.isBust()) {
-				System.out.println("Player busts!");
 				running = false;
 			}
 
@@ -56,41 +55,63 @@ public class BlackjackApp {
 
 				if (dealer.isBlackjack() && !(player.isBlackjack())) {
 					System.out.println("Dealer beats Player with Blackjack!");
-				} else {
+				}
+				if (dealer.isBlackjack() && (player.isBlackjack())) {
 					System.out.println("Push! Both Player and Dealer have Blackjack!");
 				}
 			}
-		}
-		if (player.getHandValue() < 21) {
 
-			System.out.println("Would you like to hit or hold? \n Press 1: hit \n Press 2: hold");
-			int userSelection = sc.nextInt();
+			if (player.getHandValue() < 21) {
 
-			if (userSelection == 1) {
-				System.out.println("You've chosen to hit!");
-				player.getHand().addCard(deck);
+				System.out.println("Would you like to hit or hold? \n Press 1: hit \n Press 2: hold");
+				int userSelection = sc.nextInt();
+
+				if (userSelection == 1) {
+					System.out.println("You've chosen to hit!");
+					player.getHand().addCard(deck);
+					dealer.viewPlayerHand();
+					dealer.printHandValue();
+					System.out.println(" ");
+					player.viewPlayerHand();
+					player.printHandValue();
+				}
+				if (userSelection == 2) {
+					System.out.println("You've chosen to hold.");
+					running = false;
+				}
+				if (userSelection != 1 && userSelection != 2) {
+					System.err.println(
+							"Invalid option. Your selection must be either \"1\" or \"2\": \n Press 1: to hit \n Press 2: to hold ");
+				}
+
+			}
+			if (dealer.getHandValue() < 17) {
+				dealer.getHand().addCard(deck);
 				dealer.viewPlayerHand();
 				dealer.printHandValue();
-				System.out.println(" ");
-				player.viewPlayerHand();
-				player.printHandValue();
-			}
-			if (userSelection == 2) {
-				System.out.println("You've chosen to hold.");
-				running = false;
-			}
-			if (userSelection != 1 && userSelection != 2) {
-				System.err.println(
-						"Invalid option. Your selection must be either \"1\" or \"2\": \n Press 1: to hit \n Press 2: to hold ");
-			}
 
+			}
+			if (player.getHandValue() > 21) {
+				System.out.println("Player busts! The Dealer Wins!");
+			} else if (dealer.getHandValue() > 21) {
+				System.out.println("Dealer busts! Player wins!");
+			} else if (player.getHandValue() > dealer.getHandValue()) {
+				System.out.println("Player wins with a score of: ");
+				player.printHandValue();
+				System.out.println("to");
+				dealer.printHandValue();
+			} else if (dealer.getHandValue() > player.getHandValue()) {
+				System.out.println("Dealer wins with a score of: ");
+				player.printHandValue();
+				System.out.println("to");
+				dealer.printHandValue();
+			} else if (dealer.getHandValue() == player.getHandValue()) {
+				System.out.println("Push! The Dealer and Player have tied.");
+			}
 		}
+
 	}
 
-}
+	
 
-//	
-//	public Player determineWinner() {
-//
-//		return theWinner;
-//	}
+}
